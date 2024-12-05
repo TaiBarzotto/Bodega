cdb8f43c3c3b8874c30d9413526e23f129078ee6
Tainara B. Bernardi <169108742+TaiBarzotto@users.noreply.github.com>
2024-12-05 17:27:48 -0300

    Update Clientes.h

--- Clientes.h
+++ Clientes.h
@@ -1,17 +1,26 @@
-typedef struct Cliente 
-{
+typedef struct Cliente {
     int codigo;
-    char nome_cliente[255];
-    char cpf[15]; // Utilizei o tipo char, para poder armazenar com a formatação adequada XXX.XXX.XXX-XX
+    char nome_cliente[100];
+    char cpf[15];
     int idade;
     int fiado;
-    struct Cliente *left;
-    struct Cliente *right;
-    int height;
+    struct Cliente *next; // Ponteiro para o próximo cliente
 } Cliente;
 
-typedef struct Sentinela_c
-{
-    Cliente *head;
-    Cliente *tail;
+// Estrutura para gerenciar a lista de clientes
+typedef struct S_Clientes {
+    Cliente *head; // Cabeça da lista
+    Cliente *tail; // Cauda da lista
 } S_Clientes;
+
+// Funções para gerenciar clientes
+int liberar_clientes(S_Clientes *s);
+void iniciar_cliente(S_Clientes *s);
+int formatarCPF(char *cpfEntrada, char *cpfFormatado);
+int validar_cpf(Cliente *temp, Cliente *aux);
+void cadastrar_cliente(S_Clientes *s);
+void mostrar_cliente(S_Clientes *s);
+
+// Variável global para o último código de cliente
+extern int ultimo_codigo;
+
