d87dff711b75802606ac532b459875a1304f60b6
Tainara B. Bernardi <169108742+TaiBarzotto@users.noreply.github.com>
2024-12-06 17:56:14 -0300

    Update Empresa.h

--- Empresa.h
+++ Empresa.h
@@ -1,5 +1,12 @@
-typedef struct 
+#include "Clientes.h"
+#include "Bebidas.h"
+
+typedef struct Empresa 
 {
     char cnpj[20]; // Utilizei o tipo char, para poder armazenar com a formatação adequada XX.XXX.XXX/XXXX-XX
     char nome_empresa[255];
+    //cada empresa tem seus proprios clientes e bebidas cadastrados
+    Bebida *root; 
+    Cliente *head;
+    struct Empresa *next;
 } Empresa;
