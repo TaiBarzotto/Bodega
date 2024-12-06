d6b143ea778cdc1f855aef8d36c4c7a5c643bd7c
Tainara B. Bernardi <169108742+TaiBarzotto@users.noreply.github.com>
2024-12-06 12:02:06 -0300

    Update Clientes.h

--- Clientes.h
+++ Clientes.h
@@ -4,19 +4,17 @@ typedef struct Cliente {
     char cpf[15];
     int idade;
     int fiado;
-    struct Cliente *next; // Ponteiro para o próximo cliente
+    struct Cliente *next; 
 } Cliente;
 
-// Estrutura para gerenciar a lista de clientes
 typedef struct S_Clientes {
-    Cliente *head; // Cabeça da lista
-    Cliente *tail; // Cauda da lista
+    Cliente *head;
+    Cliente *tail; 
 } S_Clientes;
 
 // Funções para gerenciar clientes
 int liberar_clientes(S_Clientes *s);
 void iniciar_cliente(S_Clientes *s);
-int formatarCPF(char *cpfEntrada, char *cpfFormatado);
 int validar_cpf(Cliente *temp, Cliente *aux);
 void cadastrar_cliente(S_Clientes *s);
 void mostrar_cliente(S_Clientes *s);
